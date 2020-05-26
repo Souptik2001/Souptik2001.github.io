5b22b193c41e80e9573ca69feee01c3c048ded46
Souptik <souptikdatta2001@gmail.com>
2020-05-26 13:38:17 +0530

    v:6 - Added contents to my tastes page

--- scripts/m_t.js
+++ scripts/m_t.js
@@ -1,6 +1,8 @@
 /*jshint esversion:6*/
 var main_b = document.getElementById("firstSec_i");
 var blogs = document.getElementById("m_b_i");
+var body = document.body;
+var c = 1;
 setInterval(checkSize, 300);
 
 function checkSize() {
@@ -14,5 +16,17 @@ function checkSize() {
         main_b.style.marginRight = "10vw";
         blogs.style.paddingLeft = "50px";
         blogs.style.paddingRight = "50px";
+    }
+}
+setInterval(changeImage, 5000);
+
+function changeImage() {
+    c++;
+    body.style.background = "linear-gradient( -90deg, rgb(0, 0, 0), rgb(0, 0, 0), rgba(0, 0, 0, 0.616), rgb(0, 0, 0), rgb(0, 0, 0)), url('./img/" + c + ".jpg')";
+    body.style.backgroundRepeat = "no-repeat";
+    body.style.backgroundSize = "contain";
+    body.style.backgroundPositionX = "center";
+    if (c == 7) {
+        c = 0;
     }
 }
