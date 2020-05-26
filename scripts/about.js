ccfcd18db0a7ff180485038eab69bd3f19419bda
Souptik <souptikdatta2001@gmail.com>
2020-05-26 23:06:22 +0530

    v:7 - Added some backgrounds, etc.

--- scripts/about.js
+++ scripts/about.js
@@ -1,18 +1,23 @@
 /*jshint esversion:6*/
 var main_b = document.getElementById("firstSec_i");
-var blogs = document.getElementById("m_b_i");
+var mine = document.getElementById("m_b_i");
+var desc = document.getElementById("desc_i");
 setInterval(checkSize, 300);
 
 function checkSize() {
     if (window.innerWidth < 510) {
         main_b.style.marginLeft = "0vw";
         main_b.style.marginRight = "0vw";
-        blogs.style.paddingLeft = "15px";
-        blogs.style.paddingRight = "15px";
+        mine.style.paddingLeft = "15px";
+        mine.style.paddingRight = "15px";
+        mine.style.display = 'block';
+        desc.style.paddingRight = '0px';
     } else {
         main_b.style.marginLeft = "10vw";
         main_b.style.marginRight = "10vw";
-        blogs.style.paddingLeft = "50px";
-        blogs.style.paddingRight = "50px";
+        mine.style.paddingLeft = "50px";
+        mine.style.paddingRight = "50px";
+        mine.style.display = 'flex';
+        desc.style.paddingRight = '80px';
     }
 }
