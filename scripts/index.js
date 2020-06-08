6e6004565c17171529d6343f8ed945d2eb28dff8
Souptik <souptikdatta2001@gmail.com>
2020-06-08 18:53:56 +0530

    v:10 - Added animation in home page

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,10 @@
 /*jshint esversion:6*/
 var main_b = document.getElementById("firstSec_i");
 var blogs = document.getElementById("blogs_i");
+var intro = document.getElementById("i_text");
+var cursor = document.getElementById("cursor");
 setInterval(checkSize, 300);
+
 
 function checkSize() {
     if (window.innerWidth < 510) {
@@ -16,3 +19,44 @@ function checkSize() {
         blogs.style.paddingRight = "50px";
     }
 }
+// function showCursor(){
+//     cursor.innerText="|";
+//     setTimeout(hideCursor, 500);
+// }
+// function hideCursor(){
+//     cursor.innerText="";
+//     setTimeout(showCursor, 500);
+// }
+// hideCursor();
+
+const text1 = ['H', 'e', 'l', 'l', 'o', '.', '.', '👱🏼'];
+var i = 14*(text1.length); // 14*number of letters
+var stat = "d";
+var wait = "t";
+var j = 0;
+function writeLetter(){
+    if(i>0 & stat=="d" ){
+        stat = "d";
+        intro.innerHTML= intro.innerHTML.slice(0, -14);
+        i=i-14;
+        j=0;
+    }
+    else{
+        stat = "i";
+        intro.innerHTML= intro.innerHTML+"<span>"+text1[(i-(14*(j)))+j]+"</span>";
+        i=i+14;
+        j= j + 1;
+    }
+    if(i==0){wait = "t"}
+    if (i==14*(text1.length)){wait="t"; stat="d";}
+    if(wait == "t"){
+        wait = "f";
+        setTimeout(writeLetter, 1000);
+    }
+    else{
+        setTimeout(writeLetter, 200);
+    }
+}
+writeLetter();
+
+
