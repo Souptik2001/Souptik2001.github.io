9ec14aa23aa2c7a08c7f0c303da608e4641719b0
Souptik <souptikdatta2001@gmail.com>
2020-12-05 21:50:33 +0530

    changed my tastes page to add scroll image

--- scripts/about.js
+++ scripts/about.js
@@ -5,12 +5,13 @@ var desc = document.getElementById("desc_i");
 setInterval(checkSize, 300);
 
 function checkSize() {
-    if (window.innerWidth < 510) {
+    if (window.innerWidth < 965) {
         main_b.style.marginLeft = "0vw";
         main_b.style.marginRight = "0vw";
         mine.style.paddingLeft = "15px";
         mine.style.paddingRight = "15px";
         mine.style.display = 'block';
+        mine.style.marginBottom = '20px';
         desc.style.paddingRight = '0px';
     } else {
         main_b.style.marginLeft = "10vw";
--- scripts/m_t.js
+++ scripts/m_t.js
@@ -1,32 +1,26 @@
 /*jshint esversion:6*/
 var main_b = document.getElementById("firstSec_i");
-var blogs = document.getElementById("m_b_i");
 var body = document.body;
-var c = 1;
-setInterval(checkSize, 300);
+var scrollvid = document.body;
+var video = document.getElementById("back_vid");
+const controller = new ScrollMagic.Controller();
 
-function checkSize() {
-    if (window.innerWidth < 510) {
-        main_b.style.marginLeft = "0vw";
-        main_b.style.marginRight = "0vw";
-        blogs.style.paddingLeft = "15px";
-        blogs.style.paddingRight = "15px";
-    } else {
-        main_b.style.marginLeft = "10vw";
-        main_b.style.marginRight = "10vw";
-        blogs.style.paddingLeft = "50px";
-        blogs.style.paddingRight = "50px";
-    }
-}
-setInterval(changeImage, 5000);
+const scene = new ScrollMagic.Scene({
+    duration: 3200, //time of video * 1000
+    triggerElement:scrollvid,
+    triggerHook:1
+}).setPin(scrollvid).addTo(controller);
 
-function changeImage() {
-    c++;
-    body.style.background = "linear-gradient( -90deg, rgb(0, 0, 0), rgb(0, 0, 0), rgba(0, 0, 0, 0.616), rgb(0, 0, 0), rgb(0, 0, 0)), url('./img/" + c + ".jpg')";
-    body.style.backgroundRepeat = "no-repeat";
-    body.style.backgroundSize = "contain";
-    body.style.backgroundPositionX = "center";
-    if (c == 7) {
-        c = 0;
-    }
-}
+var accelamount = 0.1;
+var scrollpos = 0;
+var delay =0;
+
+scene.on("update", (e)=>{
+    scrollpos = e.scrollPos/1000;
+});
+
+setInterval(()=>{
+    delay+=(scrollpos-delay)*accelamount;
+    // console.log(scrollpos, delay);
+    video.currentTime=scrollpos;
+}, 120);
