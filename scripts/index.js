cc099af1130127f8fd4e7ebd6798e41f16f1d0a5
Souptik Datta <souptikdatta2001@gmail.com>
2022-05-06 00:09:53 +0530

    fix: Better HTML structure and fix CLS issues
    Blog posts will now be displayed with proper padding and margin in mobile devides.

--- scripts/index.js
+++ scripts/index.js
@@ -3,22 +3,7 @@ var main_b = document.getElementById("firstSec_i");
 var blogs = document.getElementById("blogs_i");
 var intro = document.getElementById("i_text");
 var cursor = document.getElementById("cursor");
-setInterval(checkSize, 300);
 
-
-function checkSize() {
-    if (window.innerWidth < 510) {
-        main_b.style.marginLeft = "0vw";
-        main_b.style.marginRight = "0vw";
-        blogs.style.paddingLeft = "15px";
-        blogs.style.paddingRight = "15px";
-    } else {
-        main_b.style.marginLeft = "10vw";
-        main_b.style.marginRight = "10vw";
-        blogs.style.paddingLeft = "50px";
-        blogs.style.paddingRight = "50px";
-    }
-}
 // function showCursor(){
 //     cursor.innerText="|";
 //     setTimeout(hideCursor, 500);
