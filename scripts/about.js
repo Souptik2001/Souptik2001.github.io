b5d6e46b4e7b1d70d5e8283d64789dab91d90420
Souptik Datta <souptikdatta2001@gmail.com>
2022-05-06 01:02:19 +0530

    fix: Optimize and fix CSS in about page

--- scripts/about.js
+++ scripts/about.js
@@ -37,22 +37,6 @@ function checkSize() {
     }else{
         navbarcustom.style.backgroundColor = "rgba(0, 0, 0, 0)";
     }
-    if (window.innerWidth < 965) {
-        main_b.style.marginLeft = "0vw";
-        main_b.style.marginRight = "0vw";
-        mine.style.paddingLeft = "15px";
-        mine.style.paddingRight = "15px";
-        mine.style.display = 'block';
-        mine.style.marginBottom = '20px';
-        desc.style.paddingRight = '0px';
-    } else {
-        main_b.style.marginLeft = "10vw";
-        main_b.style.marginRight = "10vw";
-        mine.style.paddingLeft = "50px";
-        mine.style.paddingRight = "50px";
-        mine.style.display = 'flex';
-        desc.style.paddingRight = '80px';
-    }
 }
 
 function removeOtherNodes(nodeType){
