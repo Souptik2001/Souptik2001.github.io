ba50b518396a961bef1c2702aa4beb357b47f84e
Souptik Datta <souptikdatta2001@gmail.com>
2022-08-13 15:26:15 +0530

    chore: Fix footer style

--- components/Footer.js
+++ components/Footer.js
@@ -3,10 +3,17 @@ import { Box, Flex, Link } from "@chakra-ui/react";
 const Footer = (props) => {
 
 	return(
-		<Flex textAlign="center" width="100%" flexDirection={["column", null, null, null, "row"]} justifyContent="space-between" alignItems="center" {...props}>
-			<Box textAlign="center" width={["100%", null, null, null, "100%"]} fontWeight="600" fontSize="16px" lineHeight="32px" color="white">
-			Built with 💻 by <Link isExternal href="https://www.linkedin.com/in/souptik-datta-a10072183/">Souptik</Link>
-			</Box>
+		<Flex width="100%" flexDirection="column" justifyContent="center" alignItems="center" {...props}>
+			<Box
+			width="15%"
+			border="0.5px solid rgba(255, 255, 255, 0.4)"
+			marginBottom="40px"
+			></Box>
+			<Flex textAlign="center" width="100%" flexDirection={["column", null, null, null, "row"]} justifyContent="space-between" alignItems="center">
+				<Box textAlign="center" width={["100%", null, null, null, "100%"]} fontWeight="600" fontSize="16px" lineHeight="32px" color="white">
+					Built with 💻 by <Link isExternal href="https://www.linkedin.com/in/souptik-datta-a10072183/">Souptik</Link>
+				</Box>
+			</Flex>
 	  </Flex>
 	)
 
--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,5 @@
 import { Box } from "@chakra-ui/react";
+import Footer from './Footer';
 import NavBar from "./Navbar";
 
 const Layout = ({
@@ -22,6 +23,7 @@ const Layout = ({
 		>
 			<NavBar position="sticky" top="0" px={["15px", null, null, "92px"]} py={10} marginBottom={["46px", null, "70px"]} borderBottom="1px solid rgba(255, 255, 255, 0.2)" background="rgba(0, 0, 0, 0.6)" backdropFilter="blur(6px)" />
 			{ children }
+			<Footer px={["15px", null, null, "128px"]} py={["32px", null, null, "64px"]}  />
 		</Box>
 
 	);
