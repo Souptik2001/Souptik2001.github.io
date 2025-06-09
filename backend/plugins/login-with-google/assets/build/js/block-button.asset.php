0b4642fe9d96fb26aa23ca068343450df079cfa4
Souptik Datta <souptikdatta2001@gmail.com>
2025-06-09 11:15:52 +0530

    feat: Add login with google plugin
    Signed-off-by: Souptik Datta <souptikdatta2001@gmail.com>

---
+++ backend/plugins/login-with-google/assets/build/js/block-button.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-i18n'), 'version' => 'b21f02a2bdbd4c31');
---
+++ backend/plugins/login-with-google/assets/build/js/login.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '822d67fbb4662de2');
